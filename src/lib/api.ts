421c5a74bd455b348cf7e95780b11661d75d4a47
modusami <128193218+modusami@users.noreply.github.com>
2024-09-14 22:08:12 -0400

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { UserProfileFormData } from "./commons";
 
-const API_BASE_URL = process.env.NEXT_API_BASE_URL;
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const checkUserOnboarding = async (email: string) => {
 	try {
