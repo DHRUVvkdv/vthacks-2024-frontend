bc2d51abb6f019b45d797d9792dc653793c752a6
ady2303 <2303singh.aditya@gmail.com>
2024-09-15 04:07:50 -0400

    worked on features section

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,109 +1,90 @@
 import { Badge } from "./ui/badge";
-
 import { Link } from "lucide-react";
 import { CardContainer, CardBody, CardItem } from "./ui/3d-card";
 import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "./ui/card";
+import AccImage from "./AccExp.png";
+import PlannerImage from "./MyPlanner.png"
+import SettingsImage from "./Settings.png"
+import { StaticImageData } from "next/image";
 
 interface FeatureProps {
-	title: string;
-	description: string;
-	image: string;
+  title: string;
+  description: string;
+  image: string | StaticImageData;
 }
 
 const features: FeatureProps[] = [
-	{
-		title: "Multi-Dimensional Reviews",
-		description:
-			"From cleanliness to local attractions, our platform provides reviews across various categories so you can make informed decisions",
-		image: "https://static01.nyt.com/images/2024/03/10/nyregion/10nyc-comeback-01-czlv/00nyc-comeback-01-czlv-videoSixteenByNine3000.jpg",
-	},
-	{
-		title: "Traveler Insights",
-		description:
-			"Gain real insights from fellow travelers and locals, offering you a firsthand look at the destinations before you visit.",
-		image: "https://static01.nyt.com/images/2024/03/10/nyregion/10nyc-comeback-01-czlv/00nyc-comeback-01-czlv-videoSixteenByNine3000.jpg",
-	},
-	{
-		title: "Custom Ratings by Category",
-		description:
-			"Get detailed ratings across key aspects like ambiance, hospitality, food quality, and accessibility.",
-		image: "https://static01.nyt.com/images/2024/03/10/nyregion/10nyc-comeback-01-czlv/00nyc-comeback-01-czlv-videoSixteenByNine3000.jpg",
-	},
+  {
+    title: "Accessible Exploration",
+    description:
+      "This page showcases our key features designed to help users find, rate, and share information about accessible locations, empowering everyone to explore with confidence",
+    image: AccImage,
+  },
+  {
+    title: "Personalised Journey Planner",
+    description:
+	"Create your perfect trip with our AI-powered planner. We consider your unique needs, preferences, and accessibility requirements to craft a tailor-made itinerary",
+    image: PlannerImage,
+  },
+  {
+    title: "Accessibility on Your Fingertips",
+    description:
+      "From dyslexia-friendly text modes to customizable font sizes, we're committed to ensuring everyone can explore with ease and confidence",
+    image: SettingsImage,
+  },
 ];
 
 const featureList: string[] = [
-	"Dark/Light theme",
-	"Reviews",
-	"Features",
-	"Pricing",
-	"Contact form",
-	"Our team",
-	"Responsive design",
-	"Newsletter",
-	"Minimalist",
+  "Dark/Light theme",
+  "Reviews",
+  "Features",
+  "Pricing",
+  "Contact form",
+  "Our team",
+  "Responsive design",
+  "Newsletter",
+  "Minimalist",
 ];
 
 export const Features = () => {
-	return (
-		<section id="features" className="container py-20 sm:py-32 space-y-8">
-			<h2 className="text-3xl lg:text-4xl font-bold md:text-center">
-				Many{" "}
-				<span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
-					Great Features
-				</span>
-			</h2>
+  return (
+    <section id="features" className="container py-20 sm:py-32 space-y-8">
+      <h2 className="text-3xl lg:text-4xl font-bold md:text-center">
+        Many{" "}
+        <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
+          Great Features
+        </span>
+      </h2>
 
-			{/* badges of all features */}
-			<div className="flex flex-wrap md:justify-center gap-4">
-				{featureList.map((feature: string) => (
-					<div key={feature}>
-						<Badge variant="secondary" className="text-sm">
-							{feature}
-						</Badge>
-					</div>
-				))}
-			</div>
+      <div className="flex flex-wrap md:justify-center gap-4">
+        {featureList.map((feature: string) => (
+          <div key={feature}>
+            <Badge variant="secondary" className="text-sm">
+              {feature}
+            </Badge>
+          </div>
+        ))}
+      </div>
 
-			{/* old card containers */}
-			{/* <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
         {features.map(({ title, description, image }: FeatureProps) => (
           <Card key={title}>
             <CardHeader>
               <CardTitle>{title}</CardTitle>
             </CardHeader>
 
-            <CardContent>{description}</CardContent>
+            <CardContent className="min-h-28">{description}</CardContent>
 
             <CardFooter>
               <img
-                src={image}
+                src={typeof image === 'string' ? image : image.src}
                 alt="About feature"
                 className="w-[200px] lg:w-[300px] mx-auto"
               />
             </CardFooter>
           </Card>
         ))}
-      </div> */}
-
-			<div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-				{features.map(({ title, description, image }: FeatureProps) => (
-					<Card key={title}>
-						<CardHeader>
-							<CardTitle>{title}</CardTitle>
-						</CardHeader>
-
-						<CardContent className="min-h-28">{description}</CardContent>
-
-						<CardFooter>
-							<img
-								src={image}
-								alt="About feature"
-								className="w-[200px] lg:w-[300px] mx-auto"
-							/>
-						</CardFooter>
-					</Card>
-				))}
-			</div>
-		</section>
-	);
+      </div>
+    </section>
+  );
 };
