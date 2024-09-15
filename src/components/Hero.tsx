c3053286b3a8060c4d8a45bf2774f6c0c0ab8d3e
ady2303 <2303singh.aditya@gmail.com>
2024-09-15 02:56:04 -0400

    got more api stuff working

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,13 +8,13 @@ export const Hero = () => {
 	const { redirectToSignupPage } = useRedirectFunctions();
 	return (
 		<section className="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
-			<div className="text-center lg:text-start space-y-6">
+			<div className="text-center lg:text-start items-center text-center space-y-6">
 				<div className="text-5xl md:text-6xl font-bold">
-					<h1 className="inline">
-						<span className="inline bg-gradient-to-r from-[#E97451]  to-[#d25c47] text-transparent bg-clip-text ">
-							Accessibility Mapped, Adventures Unlocked
-						</span>
-					</h1>{" "}
+				<h1 className="inline text-50xl">
+				<span className="inline font-['Open_Sans'] bg-clip-text">
+					<span>map</span><span className="text-[#E97451]">ability</span>
+				</span>
+				</h1>
 					{/* <h2 className="inline">
             <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
               APP NAME
@@ -22,17 +22,16 @@ export const Hero = () => {
           </h2> */}
 				</div>
 
-				<p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
-					Explore, Experience, and Share the Best Places with In-Depth Reviews & Ratings
-					Across Categories
+				<p className="text-2xl text-muted-foreground  mx-auto lg:mx-0">
+					Accessibility Mapped, Adventures Unlocked
 				</p>
 
 				<div className="space-y-4 md:space-y-0 md:space-x-4">
 					<Button
-						className="w-full md:w-1/3 text-black font-black"
+						className="w-full md:w-1/3"
 						onClick={(e) => redirectToSignupPage()}
 					>
-						Start Your Journey
+						Begin
 					</Button>
 
 					<a
@@ -43,7 +42,7 @@ export const Hero = () => {
 							variant: "outline",
 						})}`}
 					>
-						Github Repository
+						Github
 						<GitHubLogoIcon className="ml-2 w-5 h-5" />
 					</a>
 				</div>
